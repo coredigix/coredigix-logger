c5e1b89edfcf6d4b631ce3d9cb052b227ec1faab
rafikalid <khalid.rfk@gmail.com>
2018-03-12 18:21:27 +0000

    cc

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp	= require('gulp');
 var minify	= require('gulp-minify');
 var include	= require("gulp-include");
 var rename	= require("gulp-rename");
+var voidLine= require('gulp-remove-empty-lines');
 
 gulp.task('node', () => {
 	gulp.src('assets/node.js')
@@ -13,4 +14,16 @@ gulp.task('node', () => {
 		.pipe(rename('index.js'))
 		.pipe(minify())
 		.pipe(gulp.dest("dist/"));
+});
+
+
+gulp.task('brighter', () => {
+	gulp.src('assets/brighter.js')
+		.pipe(include({
+			hardFail: true
+		}))
+		.pipe(rename('logger.js'))
+		.pipe(voidLine())
+		// .pipe(minify())
+		.pipe(gulp.dest("../brighterjs/assets/plugins/"));
 });
